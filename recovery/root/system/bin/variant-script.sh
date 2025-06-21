2f93a47c56efeec66f467da40041f6b7d956b23d
InternalHellhound <Internal.Hellhound@gmail.com>
2025-06-21 15:29:01 +0300

    pineapple: root: bin: add device variant initialization

---
+++ recovery/root/system/bin/variant-script.sh
@@ -0,0 +1,109 @@
+#!/system/bin/sh
+# This script is needed to automatically set device props.
+variant=$(getprop ro.boot.prjname)
+echo $variant
+        case $variant in
+            "23607")
+            # realme GT5 Pro (enzo)
+                echo "Realme GT5 pro" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE5C37"
+                resetprop ro.product.system.device "RMX3888"
+                resetprop ro.product.vendor.device "RE5C37"
+                resetprop ro.product.odm.device "RE5C37"
+                resetprop ro.product.product.device "RE5C37"
+                resetprop ro.product.system_ext.device "RE5C37"
+                resetprop ro.product.product.model "RMX3888"
+                resetprop ro.product.model "RMX3888"
+                resetprop ro.product.system.model "RMX3888"
+                resetprop ro.product.system_ext.model "RMX3888"
+                resetprop ro.product.vendor.model "RMX3888"
+                resetprop ro.product.odm.model "RMX3888"
+                resetprop ro.boot.hardware.revision "CN"
+                ;;
+            "23609")
+            # realme GT Neo 6SE (bale)
+                echo "Realme GT Neo 6SE" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE5C39L1"
+                resetprop ro.product.system.device "RMX3850"
+                resetprop ro.product.vendor.device "RE5C39L1"
+                resetprop ro.product.odm.device "RE5C39L1"
+                resetprop ro.product.product.device "RE5C39L1"
+                resetprop ro.product.system_ext.device "RE5C39L1"
+                resetprop ro.product.product.model "RMX3850"
+                resetprop ro.product.model "RMX3850"
+                resetprop ro.product.system.model "RMX3850"
+                resetprop ro.product.system_ext.model "RMX3850"
+                resetprop ro.product.vendor.model "RMX3850"
+                resetprop ro.product.odm.model "RMX3850"
+                resetprop ro.boot.hardware.revision "CN"
+                ;;
+            "23622")
+            # realme GT Neo6 (bale)
+                echo "Realme GT Neo6" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE5C46L1"
+                resetprop ro.product.system.device "RMX3852"
+                resetprop ro.product.vendor.device "RE5C46L1"
+                resetprop ro.product.odm.device "RE5C46L1"
+                resetprop ro.product.product.device "RE5C46L1"
+                resetprop ro.product.system_ext.device "RE5C46L1"
+                resetprop ro.product.product.model "RMX3852"
+                resetprop ro.product.model "RMX3852"
+                resetprop ro.product.system.model "RMX3852"
+                resetprop ro.product.system_ext.model "RMX3852"
+                resetprop ro.product.vendor.model "RMX3852"
+                resetprop ro.product.odm.model "RMX3852"
+                resetprop ro.boot.hardware.revision "CN"
+                ;;
+            "23631")
+            # realme GT6 CN (divo)
+                echo "Realme GT6" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE5C4FL1"
+                resetprop ro.product.system.device "RMX3800"
+                resetprop ro.product.vendor.device "RE5C4FL1"
+                resetprop ro.product.odm.device "RE5C4FL1"
+                resetprop ro.product.product.device "RE5C4FL1"
+                resetprop ro.product.system_ext.device "RE5C4FL1"
+                resetprop ro.product.product.model "RMX3800"
+                resetprop ro.product.model "RMX3800"
+                resetprop ro.product.system.model "RMX3800"
+                resetprop ro.product.system_ext.model "RMX3800"
+                resetprop ro.product.vendor.model "RMX3800"
+                resetprop ro.product.odm.model "RMX3800"
+                resetprop ro.boot.hardware.revision "CN"
+                ;;
+            "23718")
+            # realme GT6 Global (bale)
+                echo "Realme GT6" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE5CA6L1"
+                resetprop ro.product.system.device "RMX3851"
+                resetprop ro.product.vendor.device "RE5CA6L1"
+                resetprop ro.product.odm.device "RE5CA6L1"
+                resetprop ro.product.product.device "RE5CA6L1"
+                resetprop ro.product.system_ext.device "RE5CA6L1"
+                resetprop ro.product.product.model "RMX3851"
+                resetprop ro.product.model "RMX3851"
+                resetprop ro.product.system.model "RMX3851"
+                resetprop ro.product.system_ext.model "RMX3851"
+                resetprop ro.product.vendor.model "RMX3851"
+                resetprop ro.product.odm.model "RMX3851"
+                resetprop ro.boot.hardware.revision "GL"
+                ;;
+            "24687")
+            # realme GT6T (bale)
+                echo "Realme GT6T" > /config/usb_gadget/g1/strings/0x409/product
+                resetprop ro.product.device "RE606FL1"
+                resetprop ro.product.system.device "RMX3853"
+                resetprop ro.product.vendor.device "RE606FL1"
+                resetprop ro.product.odm.device "RE606FL1"
+                resetprop ro.product.product.device "RE606FL1"
+                resetprop ro.product.system_ext.device "RE606FL1"
+                resetprop ro.product.product.model "RMX3853"
+                resetprop ro.product.model "RMX3853"
+                resetprop ro.product.system.model "RMX3853"
+                resetprop ro.product.system_ext.model "RMX3853"
+                resetprop ro.product.vendor.model "RMX3853"
+                resetprop ro.product.odm.model "RMX3853"
+                resetprop ro.boot.hardware.revision "GL"
+                ;;
+esac
+exit 0
